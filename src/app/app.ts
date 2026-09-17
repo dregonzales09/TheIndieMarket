1d2fdbfc55d42444f0db43b81f5c4379d44016cb
dregonzales09 <maxwellxandre@gmail.com>
2026-09-17 19:43:54 +0800

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Components } from '@the-indie-market/components';
+
+@Component({
+  imports: [RouterOutlet],
+  selector: 'app-root',
+  styleUrl: './app.css',
+  templateUrl: './app.html',
+})
+export class App {
+  protected readonly title = signal('the-indie-market');
+}
